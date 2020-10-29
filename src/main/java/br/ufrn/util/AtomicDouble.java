d624905f31d51d8eb6bb49bab9e4001637c1f3a0
Paulo Medeiros <pauloaugusto99@ufrn.edu.br>
2020-10-29 15:30:55 -0300

    atomic double class

--- src/main/java/br/ufrn/util/AtomicDouble.java
+++ src/main/java/br/ufrn/util/AtomicDouble.java
@@ -1,4 +1,37 @@
 package br.ufrn.util;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.Double.*;
+
 public class AtomicDouble {
+
+    private AtomicLong bits;
+
+    public AtomicDouble() {
+        this(0f);
+    }
+
+    public AtomicDouble(double initialValue) {
+        bits = new AtomicLong(doubleToLongBits(initialValue));
+    }
+
+
+    public final double get() {
+        return longBitsToDouble(bits.get());
+    }
+
+    public final double getAndSet(double newValue) {
+        return longBitsToDouble(bits.getAndSet(doubleToLongBits(newValue)));
+    }
+
+    public final boolean compareAndSet(double expect, double update) {
+        return bits.compareAndSet(doubleToLongBits(expect),
+                doubleToLongBits(update));
+    }
+
+    public final double addAndGet(double value){
+        return longBitsToDouble(bits.addAndGet(doubleToLongBits(value)));
+    }
+
 }
